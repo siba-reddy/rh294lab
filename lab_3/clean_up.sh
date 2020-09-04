7e499a845861856e7df71dde1406e755a3f61f6f
siba <root@workstation.lab.example.com>
2020-09-05 02:02:55 +0530

    ansible_lab_1_2_3

---
+++ lab_3/clean_up.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo " ### I hope, you are able to finsh the guided exercise ### "
+echo " ### If you have any issue, please contact me  ### 
+
+			N Siba Kumar Reddy,
+#			Automation Consultant,
+			[ RHCSA | RHCE | ANSIBLE | ADVANCE ANSIBLE| 
+		        VIRTUALIZATION | DOCKER | OPENSHIFT | AWS | GIT ] 
+#			Complete Open Source Solutions (COSSINDIA)
+
+
+#			Contact Number:- 8249397291 (whatsApp) 
+			Mail ID:- siba.kumar@cossindia.net
+#			LinkedIn:- linkedin.com/in/n-siba-kumar-reddy 
+
+			Address:- #389, 1st Floor,
+          			  8th Main, 7th Cross,
+	  			  BTM 2nd Stage, MICO Layout
+	                          Bengaluru, Karnataka (560076)
+
+
+###############################  COSS Virtual Training ################################# "
+rm -rf   ~/deploy-manage
+
---
+++ lab_3/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "### Welcome to COSS Virtual Training ###"
+#yum install ansible -y
+
+echo "# ansible version is -->"
+
+ansible --version
+
+echo " --------------------8-------------------------
+
+#check the system connection #"
+ping -c1 servera.lab.example.com
+ping -c1 serverb.lab.example.com
+ping -c1 serverc.lab.example.com
+ping -c1 serverd.lab.example.com
+
+echo " -----------------------O-------------------
+
+##### Congratulations your Lab is ready #####"
+echo "##### In this exercise, you will create a new static inventory containing hosts and groups #####."
+
